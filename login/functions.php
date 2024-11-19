533c7fc671f6664f32bb744bdc1724381aa37a3c
Reignian <reignianc.magno@gmail.com>
2024-11-20 01:49:45 +0800

    services (customer side)

--- login/functions.php
+++ login/functions.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../functions/config.php';
+require_once __DIR__ . '/../config.php';
 
 
 $database = new Database();
--- website/services/services.class.php
+++ website/services/services.class.php
@@ -1,13 +1,26 @@
 <?php
 
-require_once __DIR__ . '/../../functions/config.php';
+require_once __DIR__ . '/../../config.php';
 
-class Profile_class{
+class Services_class{
 
     protected $db;
 
     function __construct(){
         $this->db = new Database();
     }
+
+    public function displayGymRates(){
+        $conn = $this->db->connect();
+        $sql = "SELECT p.id as program_id, p.program_name, p.price, 
+                CONCAT(p.duration, ' ', dt.type_name) as validity 
+                FROM programs p
+                LEFT JOIN duration_types dt ON p.duration_type_id = dt.id
+                WHERE p.status_id = 1";
+        $result = $conn->query($sql);
+        return $result;
+    }
+
+
 
 }
