9d69d5c8ac2039f951b21d220fabb5aab1fc5724
Jerard Regalado <162579281+ft-Jerex@users.noreply.github.com>
2024-11-21 22:52:29 +0800

    additional vendor

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="../vendor/bootstrap-5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../vendor//bootstrap-icons-1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../vendor/datatable-2.1.8/datatables.min.css" >
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="../vendor/fontawesome-free-5.15.4-web/css/all.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"> -->
     <link rel="stylesheet" href="../admin/css/navbar1.css">
     <link rel="stylesheet" href="../admin/css/admin.css">
 </head>
