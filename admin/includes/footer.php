28900c15afb4b78f4c97dc1986778c364a4abe02
Jerard Regalado <162579281+ft-Jerex@users.noreply.github.com>
2024-11-21 21:56:47 +0800

    adding vendor

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,8 +1,7 @@
-<script src="../"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="../vendor/jQuery-3.7.1/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+<script src="../vendor/bootstrap-5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="../vendor/chartjs-4.4.5/chart.js"></script>
+<script src="../vendor/datatable-2.1.8/datatables.min.js"></script>
 <script src="../admin/js/navbar.js"></script>
 <script src="../admin/js/admin.js"></script>
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gym Admin Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../vendor/bootstrap-5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../vendor//bootstrap-icons-1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../vendor/datatable-2.1.8/datatables.min.css" >
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="../admin/css/navbar1.css">
     <link rel="stylesheet" href="../admin/css/admin.css">
 </head>
